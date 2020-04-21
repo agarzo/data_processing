81afc569a96c822ce4c35b781678f129e28aa459
Garzo Manzanares <ainara.garzo@tecnalia.com>
2020-04-21 16:16:25 +0200

    Changes made to read also the participant ID and write it together with the result

--- SUS_process_class.py
+++ SUS_process_class.py
@@ -23,12 +23,16 @@ class SUS_quest:
         
         extension = ["*.csv"]
 
+        print "open the box"
+        
         fileName = eg.fileopenbox(msg="Open File",
                                  title="Control: fileopenbox",
                                  default='',
                                  filetypes=extension)
-                               
+        print "open the box2"
+                       
         eg.msgbox(fileName, "fileopenbox", ok_button="Continuar")
+        print "open the box3"
             
         with open(fileName) as File:
             reader = csv.reader(File, delimiter=',', quotechar=',',
@@ -39,27 +43,36 @@ class SUS_quest:
     
     def calculate_SUS (self):
         results = [] 
-
+        results2 = []
+        
         for val in self.csv_values[1:len(self.csv_values)]:
             results.append(self.participant_SUS (val))
-            
-        final_result = self.mean (results)
+        
+        
+        for i in range (0, len(results)): 
+            results2.append(results[i][1])
+            print "The result for participant " + str (results[i][0]) + " is: " + str (results[i][1])
+        
+        final_result = self.mean (results2)
         print "The result of SUS questionnaires is: " + str(final_result)
     
     def participant_SUS (self, val_num):
         #SUS value calculation for each participant (using each row values)
-        val_num = [item.replace(";", "") for item in val_num]
+        part_num, val_num = val_num[0].split(";", 1)
+
+        val_num = val_num.replace(";", "")
         result = 0  
+        
         i = 0      
     
-        while i < len(val_num[0]):
-            result = result + (int (val_num[0][i])-1)
-            result = result + (5-int (val_num[0][i+1]))
+        while i < len(val_num):
+            result = result + (int (val_num[i])-1)
+            result = result + (5-int (val_num[i+1]))
             
             i = i + 2
             
         result = result * 2.5
-        return result
+        return part_num, result
     
     def mean (self, SUS):
         #mean calculation for each participant's result
